3b66076f32041577afab7b08be89572764e1c6d6
xmonvr <kajakochanek@gmail.com>
2023-05-13 19:03:22 +0200

    Updated AppUserRepository

--- src/main/java/pl/shelter/shelterbackend/user/AppUserRepository.java
+++ src/main/java/pl/shelter/shelterbackend/user/AppUserRepository.java
@@ -11,10 +11,17 @@ import java.util.Optional;
 @Repository
 @Transactional(readOnly = true) //tylko do odczytu
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
     Optional<AppUser> findByEmail(String email);    // query w bazie
+
+    @Query("SELECT user.password FROM AppUser user WHERE user.email = ?1")
+    String findPasswordByEmail(String email);
+
+    @Query("SELECT user.id FROM AppUser user WHERE user.email = ?1")
+    String findIdByEmail(String email);
 
     @Transactional
     @Modifying
-    @Query("UPDATE AppUser a " + "SET a.enabled = TRUE WHERE a.email = ?1")
+    @Query("UPDATE AppUser a " + "SET a.enabled = TRUE WHERE a.email = ?1") //    ?1 odnosi się do pierwszego argumentu metody, czyli do email
     int enableAppUser(String email);
 }
