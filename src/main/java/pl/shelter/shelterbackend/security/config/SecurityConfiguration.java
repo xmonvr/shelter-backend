9e52b668e117dde1cbeb9e45d922ff39e039f63f
xmonvr <kajakochanek@gmail.com>
2023-11-14 10:56:15 +0100

    Updated SecurityFilterChain

--- src/main/java/pl/shelter/shelterbackend/security/config/SecurityConfiguration.java
+++ src/main/java/pl/shelter/shelterbackend/security/config/SecurityConfiguration.java
@@ -48,8 +48,11 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()       // wyłącza ochronę przed atakiem CSRF (Cross-Site Request Forgery)
-                .authorizeRequests()    // rozpoczyna konfigurację autoryzacji żądań HTTP
+        http
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers(
 //                        "/registration/register-admin",
                         "/animal/add-animal",
@@ -76,25 +79,22 @@ public class SecurityConfiguration {
                         "/tab/get-about-entry",
                         "/tab/get-contact-entry",
                         "/tab/get-volunteering-entry",
-                        "/order/create-order",
-                        "/oauth/create-oauth-token",
-                        "/registration/register-admin"
-                        ).permitAll() //// definiuje, które żądania są dostępne bez autoryzacji
+                        "/order/create-order"
+//                        "/oauth/create-oauth-token",        //todo wyrzucic
+//                        "/registration/register-admin"      //todo wyrzucic
+                        ).permitAll()
                 .anyRequest().authenticated()
-                .and()      /// kończy konfigurację autoryzacji i rozpoczyna konfigurację sesji
-                .sessionManagement()    // definiuje zarządzanie sesją
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // definiuje politykę tworzenia sesji (tryb bezstanowy)
-                //STATELESS - jesli uzytkownik jest zautentykowany i request jest zakonczony, to decision nie jest zapisywane
-                .and()  // kończy konfigurację sesji i rozpoczyna konfigurację uwierzytelniania
-                .authenticationProvider(authenticationProvider()) // definiuje sposób uwierzytelniania użytkowników
-                //tutaj mowimy springowi - zrob filter zanim zautentykujesz uzytkownika
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)     // dodaje filtr uwierzytelniania JWT przed domyślnym filtrem uwierzytelniania opartym na loginie i haśle
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
                 .logoutUrl("/auth/logout")
-                .addLogoutHandler(logoutHandler)       // tutaj jest zaimplementowany caly mechanizm logout
-                //lambda expression -> co ma sie stac po tym jak wyloguje sie z sukcesem
-                .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext())
-                );
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
+
         return http.build();
     }
 
